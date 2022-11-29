9b65fa62e88e316f3a6a10fa3f1fe3dcfa32131a
AnaellyN <anaellynobre24@gmail.com>
2022-11-29 13:42:22 -0300

    Melhorias na aplicação

--- src/main/java/ifrn/pi/eventos/models/Convidado.java
+++ src/main/java/ifrn/pi/eventos/models/Convidado.java
@@ -5,14 +5,18 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Convidado {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank
 	private String nome;
+	@NotBlank
 	private String rg;
+	
 	@ManyToOne
 	private Evento evento;
 
